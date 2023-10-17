ee75f4df1d6b0ae518dab44dc02d47d842769860
Simeon Baumann <sim0baum0eon@gmail.com>
2023-10-17 13:07:22 +0200

    codereview

---
+++ Assets/Scripts/Snake/InputManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Snake;
+using UnityEngine;
+# nullable enable
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+
+    public MovementDirection? GetPlayerInput(MovementDirection stepDirection)
+    {
+        MovementDirection? direction = null;
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            direction =  MovementDirection.Left;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            direction =  MovementDirection.Right;
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            direction = MovementDirection.Down;
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            direction = MovementDirection.Up;
+        }
+        
+        
+        if (direction == stepDirection.GetOppositeDirection())
+        {
+            return stepDirection;
+        }
+        
+        return direction;
+    }
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+}
--- Assets/Scripts/Snake/MovementDirection.cs
+++ Assets/Scripts/Snake/MovementDirection.cs
@@ -1,4 +1,7 @@
-﻿namespace Snake
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace Snake
 {
     public enum MovementDirection
     {
@@ -6,5 +9,44 @@
         Right,
         Down,
         Left
+    }
+
+    static class MovementDirectionMethods
+    {
+        public static Vector3 GetDirectionVector(this MovementDirection direction, float stepLength)
+        {
+            return direction switch
+            {
+                MovementDirection.Left => new Vector3(-stepLength, 0, 0),
+                MovementDirection.Right => new Vector3(stepLength, 0, 0),
+                MovementDirection.Down => new Vector3(0, 0, -stepLength),
+                MovementDirection.Up => new Vector3(0, 0, stepLength),
+                _ => new Vector3(0, 0, 0)
+            };
+        }
+
+        public static Quaternion GetRotation(this MovementDirection direction)
+        {
+            return direction switch
+            {
+                MovementDirection.Left => Quaternion.Euler(0,270,0),
+                MovementDirection.Right => Quaternion.Euler(0,90,0),
+                MovementDirection.Down => Quaternion.Euler(0,180,0),
+                MovementDirection.Up => Quaternion.Euler(0,0,0),
+                _ => Quaternion.Euler(0,0,0)
+            };
+        }
+
+        public static MovementDirection GetOppositeDirection(this MovementDirection direction)
+        {
+            return direction switch
+            {
+                MovementDirection.Left => MovementDirection.Right,
+                MovementDirection.Right => MovementDirection.Left,
+                MovementDirection.Down => MovementDirection.Up,
+                MovementDirection.Up => MovementDirection.Down,
+                _ => MovementDirection.Up // TODO Error Logging
+            };
+        }
     }
 }
